e7e4e0e37a95ebcfa3b518c60104a65c9a03bfdc
Fedor <fedoravdeev3@gmail.com>
2022-06-25 15:08:38 +0200

    save cart to localstorage

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '@/plugins/initStore.js', ssr:  false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -34,6 +35,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    // 'nuxt-vuex-localstorage'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
